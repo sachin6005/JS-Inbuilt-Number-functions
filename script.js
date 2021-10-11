5427467256ada114673da59fa8b4a1f961b51b13
sachin kumar <merugune.sachin123@gmail.com>
2021-10-11 22:08:07 +0530

    inbuilt number functions in javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+var num1 = 20
+var num2 = 20.55
+
+// tostring
+
+console.log(num1.toString())
+console.log(typeof(num2.toString()))
+
+var strNum1 = '10'
+var strNum2 = '22.55'
+
+// parseInt
+
+console.log(parseInt(strNum1))
+console.log(parseInt(strNum2))
+
+//parseFloat
+
+console.log(parseFloat(strNum2))
+
+var  strNum3 =  15.543672888
+ 
+// toFixed
+
+console.log(parseFloat(strNum3))
+console.log(strNum3.toFixed(2))
+console.log(strNum3.toFixed(10))
